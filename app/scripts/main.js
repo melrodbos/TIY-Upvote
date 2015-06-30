8bb00d75f390f6ae4ec61d17c5f3c4a05d606929
Michael Staehling <Michael.staehling@gmail.com>
2015-06-30 16:53:58 -0400

    edit app.config code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,31 @@
 (function() {
 
-var app = angular.module('tiy-upvote', ['ngRoute']);
+  var app = angular.module('tiy-upvote', ['ngRoute']);
 
-app.config(function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'views/home.html',
-    controller: 'HomeController',
-    controllerAs: 'home'
-  })
-});
-app.config(function($routeProvider) {
-  $routeProvider.when('/questions', {
-    templateUrl: 'views/questions.html',
-    controller: 'QuestionsController',
-    controllerAs: 'questions'
-  })
-});
-app.config(function($routeProvider) {
-  $routeProvider.when('/ask-question', {
-    templateUrl: 'views/ask-question.html',
-    controller: 'AskController',
-    controllerAs: 'ask'
-  })
-});
+  app.config(function($routeProvider) {
+    $routeProvider.when('/home', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeController',
+      controllerAs: 'home'
 
-});
+    });
+    $routeProvider.when('/questions', {
+      templateUrl: 'views/questions.html',
+      controller: 'QuestionsController',
+      controllerAs: 'questions'
+    });
+    $routeProvider.when('/ask-question', {
+      templateUrl: 'views/ask-question.html',
+      controller: 'AskController',
+      controllerAs: 'ask'
+    });
+  });
+
+  // incase a MainController is needed
+
+  app.controller('MainController'function() {
+
+  })
 
 
 
