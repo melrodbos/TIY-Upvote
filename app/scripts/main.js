ed7201cdd67278237ce490292d1e90403a4b42f7
Michael Staehling <Michael.staehling@gmail.com>
2015-06-30 17:03:50 -0400

    added a maincontroller in case 1 is needed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,6 +24,19 @@
   // incase a MainController is needed
 
   app.controller('MainController'function() {
+    this.page = function(name) {
+      this.view = 'views/404.html';
+      if (name == 'home') {
+        this.view = 'views/home.html';
+      }
+      if (name == 'questions') {
+        this.view = 'views/questions.html';
+      }
+      if (name == 'ask') {
+        this.view = 'views/ask-questions.html';
+      }
+    }
+    this.page('home');
 
   })
 
