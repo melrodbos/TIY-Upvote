e775fc2e29c08fb63161f98e491f17ac21c6b2b3
Michael Staehling <Michael.staehling@gmail.com>
2015-06-30 16:29:27 -0400

    added blank html files to potentially work in, set up  for them

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,30 @@
-(function(){
+(function() {
 
-var app = angular.module('tiy-upvote', [ ]);
+var app = angular.module('tiy-upvote', ['ngRoute']);
 
+app.config(function($routeProvider) {
+  $routeProvider.when('/home', {
+    templateUrl: 'views/home.html',
+    controller: 'HomeController',
+    controllerAs: 'home'
+  })
+});
+app.config(function($routeProvider) {
+  $routeProvider.when('/questions', {
+    templateUrl: 'views/questions.html',
+    controller: 'QuestionsController',
+    controllerAs: 'questions'
+  })
+});
+app.config(function($routeProvider) {
+  $routeProvider.when('/ask-question', {
+    templateUrl: 'views/ask-question.html',
+    controller: 'AskController',
+    controllerAs: 'ask'
+  })
+});
 
+});
 
 
 
