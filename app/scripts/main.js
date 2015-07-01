dfacd6239abab5ae8fd086e661305d2bb69ba3cb
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-07-01 17:25:08 -0400

    eslint error addressed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,24 @@
+/*global angular*/
+/* TODO: tell eslint about angular http://eslint.org/docs/rules/no-undef.html*/
 (function() {
   'use strict';
 
 
-  var app = angular.module('tiy-upvote', ['ngRoute', 'restangular' ]);
+  var app = angular.module('tiy-upvote', ['ngRoute', 'restangular']);
 
   app.config(function($routeProvider) {
     $routeProvider.when('/questions', {
-      templateUrl: '../views/questions.html'
+      templateUrl: 'views/questions.html'
       // controller: 'QuestionsController',
       // controllerAs: 'questions'
     });
     $routeProvider.when('/answer', {
-      templateUrl: '../views/answer.html'
+      templateUrl: 'views/answer.html'
       // controller: 'AnswerController',
       // controllerAs: 'answer'
     });
     $routeProvider.when('/ask', {
-      templateUrl: '../views/ask.html'
+      templateUrl: 'views/ask.html'
       // controller: 'AskController',
       // controllerAs: 'ask'
     });
