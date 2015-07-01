4cd0fb0585c99977de3a5b64a5de1707f09c41b8
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-07-01 18:21:17 -0400

    removed trailing commas from main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,5 @@
-/* global angular */
+/*global angular*/
+/*TODO: remind eslint about angular http://eslint.org/docs/rules/no-undef.html*/
 (function() {
  'use strict';
 
@@ -6,17 +7,17 @@
 
   app.config(function($routeProvider) {
     $routeProvider.when('/questions', {
-      templateUrl: '/views/questions.html',
+      templateUrl: '/views/questions.html'
       // controller: 'QuestionsController',
       // controllerAs: 'questions'
     });
     $routeProvider.when('/answer', {
-      templateUrl: '/views/answer.html',
+      templateUrl: '/views/answer.html'
       // controller: 'AnswerController',
       // controllerAs: 'answer'
     });
     $routeProvider.when('/ask', {
-      templateUrl: '/views/ask.html',
+      templateUrl: '/views/ask.html'
       // controller: 'AskController',
       // controllerAs: 'ask'
     });
