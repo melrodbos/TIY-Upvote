85d061ee321a65eabd73846a37c5d636afec67eb
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-07-01 09:57:13 -0400

    working on styles and wiring up JavaScript to switch page views

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,45 +1,46 @@
 (function() {
 
+
   var app = angular.module('tiy-upvote', ['ngRoute']);
 
   app.config(function($routeProvider) {
     $routeProvider.when('/home', {
-      templateUrl: 'views/home.html',
+      templateUrl: '../index.html',
       controller: 'HomeController',
       controllerAs: 'home'
 
     });
-    $routeProvider.when('/questions', {
-      templateUrl: 'views/questions.html',
-      controller: 'QuestionsController',
-      controllerAs: 'questions'
+    $routeProvider.when('/answer', {
+      templateUrl: '../views/answer.html',
+      controller: 'AnswerController',
+      controllerAs: 'answer'
     });
     $routeProvider.when('/ask-question', {
-      templateUrl: 'views/ask-question.html',
+      templateUrl: '../views/ask.html',
       controller: 'AskController',
       controllerAs: 'ask'
     });
   });
 
-  // incase a MainController is needed
-
+// in case a MainController is needed
+/*
   app.controller('MainController'function() {
     this.page = function(name) {
       this.view = 'views/404.html';
       if (name == 'home') {
         this.view = 'views/home.html';
       }
-      if (name == 'questions') {
-        this.view = 'views/questions.html';
+      if (name == 'answer') {
+        this.view = 'views/answer.html';
       }
       if (name == 'ask') {
-        this.view = 'views/ask-questions.html';
+        this.view = 'views/ask.html';
       }
     }
     this.page('home');
 
   })
-
+*/
 
 
 
