a354b2925998c84d81a1ca85c266233a35abc584
Nick Pope <pope410211@gmail.com>
2015-07-02 23:23:09 -0400

    fixing login button issue

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,49 +1,47 @@
 /*global angular Firebase*/
 /*TODO: remind eslint about angular http://eslint.org/docs/rules/no-undef.html*/
 (function() {
- 'use strict';
+  angular.module('tiy-upvote', ['ngRoute', 'restangular' ])
+    .config(function($routeProvider) {
+      $routeProvider.when('/questions', {
+        templateUrl: 'views/questions.html',
+        // controller: 'QuestionsController',
+        // controllerAs: 'questions'
+      });
+      $routeProvider.when('/answer', {
+        templateUrl: '../views/answer.html',
+        // controller: 'AnswerController',
+        // controllerAs: 'answer'
+      });
+      $routeProvider.when('/ask', {
+        templateUrl: '../views/ask.html',
+        // controller: 'AskController',
+        // controllerAs: 'ask'
+      });
+      $routeProvider.otherwise('/questions');
+    }) // END config($routeProvider)
+    .config(function(RestangularProvider){
 
-  var app = angular.module('tiy-upvote', ['ngRoute', 'restangular' ]);
-
-  app.config(function($routeProvider) {
-    $routeProvider.when('/questions', {
-      templateUrl: '/views/questions.html'
-      // controller: 'QuestionsController',
-      // controllerAs: 'questions'
-    });
-    $routeProvider.when('/answer', {
-      templateUrl: '/views/answer.html'
-      // controller: 'AnswerController',
-      // controllerAs: 'answer'
-    });
-    $routeProvider.when('/ask', {
-      templateUrl: '/views/ask.html'
-      // controller: 'AskController',
-      // controllerAs: 'ask'
-    });
-    $routeProvider.otherwise('/questions');
-  });
-
-  // start of firebase...
-  .contoller('MainController', function($scope){
-  var firebase = new Firebase('https://intense-torch-7857.firebaseio.com');
-
-  var self = this;
-
-  this.login = function(){
-    firebase.authWithOAuthPopup('github', function(error, auth){
-      
     })
-  }
-});
+    .controller('MainController', function($scope){
+      var firebase = new Firebase('https://glowing-inferno-3596.firebaseio.com');
 
-// $http.get('/api/questions.json')
-//
-// app.controller('ListController', function($http){
-//   var self = this;
-//   self.questions = []; //not sure if this is the right way to get things to populate?
-//
-// });
+      var self = this;
 
+      this.login = function(){
+        firebase.authWithOAuthPopup('github', function(error, auth){
+          $scope.$apply(function(){
+            self.username = auth.github.username
+          });
+        });
+      } // END login
+    }) // END controller(MainController)
+  ; // END module('tiy-upvote')
+
+  // app.controller(..., function(){
+  //   // 150 lines or so... Where was I again?
+  // }); // END controller(...)
+  //
+  // app.controller(...);
 
 })(window);
