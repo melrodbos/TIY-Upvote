653da487225414b066cf47c5dd19ed41e6c517f9
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-07-04 23:30:48 -0400

    askController restructure

--- app/scripts/tiy-upvote/ask_question.js
+++ app/scripts/tiy-upvote/ask_question.js
@@ -1,35 +1,75 @@
-// Get a database reference to our posts
-var ref = new Firebase("https://docs-examples.firebaseio.com/web/saving-data/fireblog/posts");
 
-// Attach an asynchronous callback to read the data at our posts reference
-ref.on("value", function(snapshot) {
-  console.log(snapshot.val());
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
+'use strict';
+//Ask a question controller...
+app.cotroller("AskController", function($scope){
+  //substitute "product" for "question"
+  this.question = {};
+// Push review onto this "ask" questions "array"...
+  this.addQuestion = function(ask) {
+    ask.questions.push(this.question);
+    this.question = {};
+  };
+
 });
 
-//Ask a question controller...
-// $.ajax('https://glowing-inferno-3333.firebaseio.com/foo.json', {
-//   method: ‘PUT'
-// data: '[ … ]'
+
+// app.controller("AskController", function($scope) {
+//   $scope.post = {title: '', question: ''};
+//
+// });
+//Start of Route...
+.config(function($routeProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/ask.html',
+      controller: 'AskController'
+    })
+});//End of Route...
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Get a database reference to our posts
+// var ref = new Firebase("https://docs-examples.firebaseio.com/web/saving-data/fireblog/posts");
+//
+// // Attach an asynchronous callback to read the data at our posts reference
+// ref.on("value", function(snapshot) {
+//   console.log(snapshot.val());
+// }, function (errorObject) {
+//   console.log("The read failed: " + errorObject.code);
+// });
+//
+// //Ask a question controller...
+// // $.ajax('https://glowing-inferno-3333.firebaseio.com/foo.json', {
+// //   method: ‘PUT'
+// // data: '[ … ]'
+// // }).then(tracer)
+//
+// $.ajax('http://intense-torch-7857.firebaseio.com/answers.json', {
+//   method: 'GET'
+//   data: '[ ]'
 // }).then(tracer)
-
-$.ajax('http://intense-torch-7857.firebaseio.com/answers.json', {
-  method: 'GET'
-  data: '[ ]'
-}).then(tracer)
-
-
-
-  app.controller('AskController', function($scope) {
-    var firebase = new Firebase('http://intense-torch-7857.firebaseio.com');
-    var ask = this;
-
-
-    ask = [];
-    $http.post(firebase);
-    .then(function(data) {
-
-      this.ask = {};
-    };
-  });
+//
+//
+//
+//   app.controller('AskController', function($scope) {
+//     var firebase = new Firebase('http://intense-torch-7857.firebaseio.com');
+//     var ask = this;
+//
+//
+//     ask = [];
+//     $http.post(firebase);
+//     .then(function(data) {
+//
+//       this.ask = {};
+//     };
+//   });
